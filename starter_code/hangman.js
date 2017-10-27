8c449945ef770520572ce51ca840be002e647c1a
Alfonso Moncada <alfonsomcd93@gmail.com>
2017-10-27 15:27:46 -0400

    Hangman game 1st commit

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,44 @@
 var hangman;
-
+var words = ["hello", "ironhack", "javascript", "coding"];
+var clickedLetters = [];
 function Hangman() {
-
+  this.words = words;
+  this.secretWord ="";
+  this.clickedLetters = clickedLetters;
 }
 
-Hangman.prototype._getWord = function () {
-
+Hangman.prototype._getWord = function() {
+  var ranW = (Math.floor(Math.random() * this.words.length));
+  this.secretWord = this.words[ranW];
+  return this.secretWord;
 };
 
 Hangman.prototype._checkIfLetter = function(keyCode) {
-
+  if (keyCode >= 65 && keyCode <= 90)
+  return true;
+  else
+  return false;
 };
 
 Hangman.prototype._checkClickedLetters = function(key) {
-
+ if(this.clickedLetters.indexOf(key.toUpper === -1)){
+    return true;
+    }
+    else{
+    return false;
+    }
 };
 
-Hangman.prototype._addCorrectLetter = function(i){
+Hangman.prototype._addCorrectLetter = function(i) {};
 
-};
+Hangman.prototype._addWrongLetter = function(letter) {};
 
-Hangman.prototype._addWrongLetter = function (letter){
+Hangman.prototype._checkGameOver = function() {};
 
-};
+Hangman.prototype._checkWinner = function() {};
 
-Hangman.prototype._checkGameOver = function() {
-
-};
-
-Hangman.prototype._checkWinner = function() {
-
-};
-
-document.getElementById("start-game-button").onclick = function(){
+document.getElementById("start-game-button").onclick = function() {
   hangman = new Hangman();
 };
 
-
-document.onkeydown = function(e) {
-
-};
+document.onkeydown = function(e) {};
